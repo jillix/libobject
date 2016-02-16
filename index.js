9cc41b3f1253f61323304f664029e8aaa9d19108
Adrian Ottiker <adrian@ottiker.com>
2016-02-16 09:32:23 +0000

    add object utils methods

--- index.js
+++ index.js
@@ -1,1 +1,95 @@
-exports.path = function () {};
+/**
+ * Get a value from a property "path" (dot.notation).
+ * path('Object.key.key.value', {'key': {'key': {'value': 123}}});
+ *
+ * @public
+ * @param {string} The path in "dot" notation.
+ * @param {array} The data objects, which are used to search the path.
+ */
+exports.path = function getPathValue (path, object) {
+
+    if (!path || !object) {
+        return;
+    }
+
+    // prepare path
+    path = path.split('.');
+
+    // find keys in paths or return
+    for (var i = 0; i < path.length; ++i) {
+        if ((object = object[path[i]]) === undefined) {
+            return;
+        }
+    }
+
+    return object;
+};
+
+/**
+ * Create a flat object {key1: {key2: "value"}} => {"key1.key2": "value"}
+ *
+ * @public
+ * @param {string} The object, which is flattened.
+ */
+exports.flat = function (object) {
+    var output = {};
+    var value;
+    var newKey;
+
+    // recusrive handler
+    function step(obj, prev) {
+        for (var key in obj) {
+            value = obj[key];
+            newKey = prev + key;
+
+            if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
+
+                if (Object.keys(value).length) {
+                    step(value, newKey + '.');
+                    continue;
+                }
+            }
+
+            output[newKey] = value;
+        }
+    }
+
+    // start recursive loop
+    step(object, '');
+
+    return output;
+};
+
+/**
+ * Unflatten dot-notation keys {"key1.key2": "value"} => {key1: {key2: "value"}}
+ *
+ * @public
+ * @param {string} The object, which is unflattened.
+ */
+exports.deep = function (object) {
+    var result = {};
+    var parentObj = result;
+    var key;
+    var subkeys;
+    var subkey;
+    var last;
+    var keys = Object.keys(object);
+
+    for (var i = 0; i < keys.length; ++i) {
+
+        key = keys[i];
+        subkeys = key.split('.');
+        last = subkeys.pop();
+
+        for (var ii = 0; ii < subkeys.length; ++ii) {
+            subkey = subkeys[ii];
+            parentObj[subkey] = parentObj[subkey] === undefined ? ((subkeys[ii + 1] || last) === "0" ? [] : {}) : parentObj[subkey];
+            parentObj = parentObj[subkey];
+        }
+
+        parentObj[last] = object[key];
+        parentObj = result;
+    }
+
+    return result;
+};
