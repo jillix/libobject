7cb999387198c39b453dd2a2f9b8ed2abb25601c
Adrian Ottiker <adrian@ottiker.com>
2016-10-24 20:32:39 +0200

    implement new "queue" method

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ exports.change = require('./lib/change');
 exports.path = require('./lib/path');
 exports.deep = require('./lib/deep');
 exports.flat = require('./lib/flat');
+exports.queue = require('./lib/queue');
